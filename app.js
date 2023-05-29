ce611321f4f3bdae6da6898c8dc8d0ef8ef99ebd
VelkovIv <ivaylo.velkov@gmail.com>
2023-05-29 20:58:39 +0300

    update gitignore

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-var secret = 'Thisisourlittelesecret.'; // process.env.SOME_LONG_UNGUESSABLE_STRING;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 const User = new mongoose.model('User', userSchema);
